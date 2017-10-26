c1f34b05d5b4693e76b7df5d84941d28c94f3797
ulfik <32269434+ulfik@users.noreply.github.com>
2017-10-27 00:05:29 +0200

    Update brick stack.js

--- brick stack.js
+++ brick stack.js
@@ -1,15 +1,16 @@
 var brickStack=[];
-
+var limit = 5;
 
 function addBrick(){
-  var pic = document.createElement("IMG");
-  pic.setAttribute("src", "https://terraria.gamepedia.com/media/terraria.gamepedia.com/0/05/Red_Brick_%28placed%29.png?version=60eb8d0cd23dd0a2e844c343182c2d99");
-  document.body.appendChild(pic);
-  var stackSize = brickStack.push("brick");
+    if (brickStack.length < limit){
+        brickStack.push("brick");
+        var pic = document.createElement("IMG");
+        pic.setAttribute("src", "brick.png");
+        document.getElementById("stack").appendChild(pic);
+    }else{    
+    }
 }
 
 function removeBrick(){
-  var removedBrick = brickStack.pop();
+    var removedBrick = brickStack.pop();
 }
-
-
